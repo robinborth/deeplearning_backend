f462833cab42631715d3d7213139936dca9f7593
Robin Borth <borth.private@gmail.com>
2021-02-13 17:32:25 +0100

    finish

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
-from flask import Flask
-from flask_cors import CORS
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
-CORS(app)
 
 
 def load_mnist_model():
     return tf.keras.models.load_model('saved_model/mnist_model')
 
-def convert_image_to_numpy_array(image):
-    #image = np.zeros((28,28))
-    #image[:, 15] = np.full((1,), 1)
-    #image[:, 14] = np.full((1,), 1)
-    #image[:, 13] = np.full((1,), 1)
-    #image = image.reshape(-1, 28,28,1)
-    #TODO: Convert the image o
+
+def convert_json_to_numpy_array(json):
+    image = np.array(json['image'])
+    image = image.reshape(-1, 28, 28, 1)
     return image
+
 
 def predict_image(image, model):
     predict = model.predict(image)
     return np.argmax(predict)
 
+
 @app.route('/mnist/api/v1.0/model', methods=['POST'])
 def evaluate_model():
-    request = request.get_json()
-    image = ''
-    print(request)
+    json = request.get_json()
+    image = convert_json_to_numpy_array(json)
     model = load_mnist_model()
-    image = convert_image_to_numpy_array(image)
     prediction = predict_image(image, model)
+    print(prediction)
+    return jsonify(int(prediction))
 
 
 if __name__ == '__main__':
